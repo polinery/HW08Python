b1f7212fbef8b1505c9fe1140d50190832755518
polinery <polinery@bk.ru>
2022-12-19 02:07:42 +0300

    Задача3

--- HW_08_03.py
+++ HW_08_03.py
@@ -1,1 +1,21 @@
-''' 3. '''
+''' 3. Создайте собственный класс-исключение, обрабатывающий ситуацию деления
+на нуль.
+Проверьте его работу на данных, вводимых пользователем.
+При вводе пользователем нуля в качестве делителя программа должна корректно
+обработать эту ситуацию и не завершиться с ошибкой. '''
+
+
+class DivisionByNull:
+    def __init__(self, divider, denominator):
+        self.divider = divider
+        self.denominator = denominator
+
+    def divide_by_null(divider, denominator):
+        try:
+            return (divider / denominator)
+        except:
+            return (f"Деление на ноль недопустимо")
+
+
+print(DivisionByNull.divide_by_null(int(input('Делимое: ')),
+                                    int(input('Делитель: '))))
